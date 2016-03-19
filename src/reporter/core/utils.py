c7a04df370547b4fc258af2998b2c09814bbd6ce
Robert Barsch <barsch@egu.eu>
2016-03-19 12:18:20 +0100

    remove unneeded managment command

--- src/reporter/core/utils.py
+++ src/reporter/core/utils.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-import os
 import re
 import urllib2
 
-from django.db import connection
 from django.utils.html import escape
 from lxml import etree
-
-from reporter.core import models
 
 
 def parse_report_xml(xml):
@@ -40,62 +35,6 @@ def parse_report_xml(xml):
     except:
         kwargs['node'] = None
     return kwargs
-
-
-def import_old_reports():
-    """
-    Imports reports from old SQLite database.
-    """
-    # import only if no report exist yet
-    if models.Report.objects.count() > 0:
-        print "Initial reports already imported ..."
-        return
-    else:
-        print "Initial import of old reports ..."
-    # parse old database
-    import sqlite3
-    c = sqlite3.connect(os.path.join(os.path.dirname(__file__), 'reporter.db'))
-    results = c.execute("""
-        SELECT id, timestamp, tests, errors, failures, modules, system,
-            architecture, version, xml, node
-        FROM report""")
-    # bulk create objects
-    bulk = []
-    for r in results:
-        # parse XML
-        kwargs = parse_report_xml(r[9])
-        kwargs['node'] = r[10]
-        # create report
-        report = models.Report(id=r[0], datetime=datetime.fromtimestamp(r[1]),
-            tests=r[2], errors=r[3], failures=r[4], modules=r[5], system=r[6],
-            architecture=r[7], version=r[8], xml=r[9], **kwargs)
-        print ' *', report
-        bulk.append(report)
-    models.Report.objects.bulk_create(bulk)
-    # reset primary index
-    # http://jesiah.net/post/23173834683/postgresql-primary-key-syncing-issues
-    cursor = connection.cursor()
-    cursor.execute("SELECT setval('core_report_id_seq', \
-        (SELECT MAX(id) FROM core_report)+1)")
-    print 'Initial import finished ...'
-
-
-def set_default_selected_nodes():
-    """
-    Imports reports from old SQLite database.
-    """
-    # import only if no report exist yet
-    if models.SelectedNode.objects.count() > 0:
-        print "Initial selected nodes already imported ..."
-        return
-    else:
-        print "Creating some selected nodes ..."
-    # create default nodes
-    for name in ['travis-ci', 'sphinx']:
-        obj = models.SelectedNode(name=name)
-        obj.save()
-        print ' *', obj
-    print 'Selected nodes have been created ...'
 
 
 def replace_backslashes(match):
