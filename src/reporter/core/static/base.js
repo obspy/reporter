062bf2ac846ab67efea98903dd67ac0a7236d29e
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-09-05 18:28:31 +0200

    adding github feed

--- src/reporter/core/static/base.js
+++ src/reporter/core/static/base.js
@@ -1,3 +1,13 @@
 $(window).load(function() {
+
+    console.log($("#feed"));
+    GitHubActivity.feed({
+        username: "obspy",
+        repository: "obspy", // optional
+        selector: "#github-feed",
+        limit: 20 // optional
+    });
+
     $(".twentytwenty-container").twentytwenty();
+
 });
